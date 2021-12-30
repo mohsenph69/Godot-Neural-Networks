e0ebf13c8b7b26766d5cfca071dcfd525d1b7dd2
Elizabeth Suehr <elizabeth.suehr@gmail.com>
2021-12-30 15:21:07 -0800

    Working Verison

---
+++ godot-cpp-master/README.md
@@ -0,0 +1,94 @@
+# godot-cpp
+
+This repository contains the  *C++ bindings* for the [**Godot Engine**](https://github.com/godotengine/godot)'s GDExtensions API.
+
+- [**Versioning**](#versioning)
+- [**Contributing**](#contributing)
+- [**Getting Started**](#getting-started)
+- [**Included Example**](#included-example)
+
+## Versioning
+
+This repositories follows the same branch versioning as the main [Godot Engine
+repository](https://github.com/godotengine/godot):
+
+- `master` tracks the current development branch.
+- `3.x` tracks the development of the next 3.x minor release.
+- Other versioned branches (e.g. `3.3`, `3.2`) track the latest stable release
+  in the corresponding branch.
+
+Stable releases are also tagged on this repository:
+[**Tags**](https://github.com/godotengine/godot-cpp/tags).
+
+**For any project built against a stable release of Godot, we recommend using
+this repository as a Git submodule, checking out the specific tag matching your
+Godot version.**
+
+> As the `master` and `3.x` branches are constantly getting updates, if you are
+> using `godot-cpp` against a more current version of Godot, see the instructions
+> in [**godot-headers**](https://github.com/godotengine/godot-headers) for
+> updating the relevant files.
+
+## Contributing
+
+We greatly appreciate help in maintaining and extending this project. If you
+wish to help out, ensure you have an account on GitHub and create a "fork" of
+this repository. Rémi "Akien" Verschelde wrote an excellent bit of documentation
+for the main Godot project on this:
+[Pull request workflow](https://docs.godotengine.org/en/stable/community/contributing/pr_workflow.html)
+
+Please install clang-format and copy the files in `misc/hooks` into `.git/hooks`
+so formatting is done before your changes are submitted.
+
+## Getting Started
+
+It's a bit similar to what it was for 3.x but also a bit different. This new approach is much more akin to how core Godot modules are structured.
+
+Compiling this repository generates a static library to be linked with your shared lib,
+just like before.
+
+To use the shared lib in your Godot project you'll need a `.gdextension` file, which replaces what was the `.gdnlib` before. Follow the example:
+
+```ini
+[configuration]
+
+entry_symbol = "example_library_init"
+
+[libraries]
+
+linux.64 = "bin/x11/libgdexample.so"
+windows.64 = "bin/win64/libgdexample.dll"
+```
+
+The `entry_symbol` is the name of the function that initializes your library. It should be similar to following layout:
+
+```cpp
+extern "C" {
+
+// Initialization.
+
+GDNativeBool GDN_EXPORT example_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+	init_obj.register_scene_initializer(register_example_types);
+	init_obj.register_scene_terminator(unregister_example_types);
+
+	return init_obj.init();
+}
+}
+```
+
+The `register_example_types()` should register the classes in ClassDB, very like a Godot module would do.
+
+```cpp
+using namespace godot;
+void register_example_types() {
+	ClassDB::register_class<Example>();
+}
+```
+
+Any node and resource you register will be available in the corresponding `Create...` dialog. Any class will be available to scripting as well.
+
+## Included Example
+
+Check the project in the `test` folder for an example on how to use and register different things.
---
+++ godot-cpp-master/include/godot_cpp/classes/wrapped.hpp
@@ -0,0 +1,196 @@
+/*************************************************************************/
+/*  wrapped.hpp                                                          */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef GODOT_CPP_WRAPPED_HPP
+#define GODOT_CPP_WRAPPED_HPP
+
+#include <godot_cpp/core/memory.hpp>
+
+#include <godot_cpp/godot.hpp>
+
+namespace godot {
+
+typedef void GodotObject;
+
+// Base for all engine classes, to contain the pointer to the engine instance.
+class Wrapped {
+	friend class GDExtensionBinding;
+	friend void postinitialize_handler(Wrapped *);
+
+protected:
+	virtual const char *_get_class() const = 0; // This is needed to retrieve the class name before the godot object has its _extension and _extension_instance members assigned.
+	virtual const GDNativeInstanceBindingCallbacks *_get_bindings_callbacks() const = 0;
+
+	void _postinitialize();
+
+	Wrapped(const char *p_godot_class);
+	Wrapped(GodotObject *p_godot_object);
+
+public:
+	// Must be public but you should not touch this.
+	GodotObject *_owner = nullptr;
+};
+
+} // namespace godot
+
+#ifdef DEBUG_ENABLED
+#define CHECK_CLASS_CONSTRUCTOR(m_constructor, m_class)                                                      \
+	if (unlikely(!m_constructor)) {                                                                          \
+		ERR_PRINT_ONCE("Constructor for class " #m_class "not found. Likely wasn't registered in ClassDB."); \
+		return nullptr;                                                                                      \
+	} else                                                                                                   \
+		((void)0)
+#else
+#define CHECK_CLASS_CONSTRUCTOR(m_constructor, m_class)
+#endif
+
+#define GDCLASS(m_class, m_inherits)                                                                               \
+private:                                                                                                           \
+	void operator=(const m_class &p_rval) {}                                                                       \
+	friend class ClassDB;                                                                                          \
+                                                                                                                   \
+	using SelfType = m_class;                                                                                      \
+                                                                                                                   \
+protected:                                                                                                         \
+	virtual const char *_get_class() const override {                                                              \
+		return get_class_static();                                                                                 \
+	}                                                                                                              \
+                                                                                                                   \
+	virtual const GDNativeInstanceBindingCallbacks *_get_bindings_callbacks() const override {                     \
+		return &___binding_callbacks;                                                                              \
+	}                                                                                                              \
+                                                                                                                   \
+	static void (*_get_bind_methods())() {                                                                         \
+		return &m_class::_bind_methods;                                                                            \
+	}                                                                                                              \
+                                                                                                                   \
+	template <class T>                                                                                             \
+	static void register_virtuals() {                                                                              \
+		m_inherits::register_virtuals<T>();                                                                        \
+	}                                                                                                              \
+                                                                                                                   \
+public:                                                                                                            \
+	static void initialize_class() {                                                                               \
+		static bool initialized = false;                                                                           \
+		if (initialized) {                                                                                         \
+			return;                                                                                                \
+		}                                                                                                          \
+		m_inherits::initialize_class();                                                                            \
+		if (m_class::_get_bind_methods() != m_inherits::_get_bind_methods()) {                                     \
+			_bind_methods();                                                                                       \
+			m_inherits::register_virtuals<m_class>();                                                              \
+		}                                                                                                          \
+		initialized = true;                                                                                        \
+	}                                                                                                              \
+                                                                                                                   \
+	static const char *get_class_static() {                                                                        \
+		return #m_class;                                                                                           \
+	}                                                                                                              \
+                                                                                                                   \
+	static const char *get_parent_class_static() {                                                                 \
+		return #m_inherits;                                                                                        \
+	}                                                                                                              \
+                                                                                                                   \
+	static GDNativeObjectPtr create(void *data) {                                                                  \
+		m_class *new_object = memnew(m_class);                                                                     \
+		return new_object->_owner;                                                                                 \
+	}                                                                                                              \
+                                                                                                                   \
+	static void free(void *data, GDExtensionClassInstancePtr ptr) {                                                \
+		if (ptr) {                                                                                                 \
+			m_class *cls = reinterpret_cast<m_class *>(ptr);                                                       \
+			cls->~m_class();                                                                                       \
+			::godot::Memory::free_static(cls);                                                                     \
+		}                                                                                                          \
+	}                                                                                                              \
+                                                                                                                   \
+	static void *___binding_create_callback(void *p_token, void *p_instance) {                                     \
+		return nullptr;                                                                                            \
+	}                                                                                                              \
+	static void ___binding_free_callback(void *p_token, void *p_instance, void *p_binding) {                       \
+	}                                                                                                              \
+	static GDNativeBool ___binding_reference_callback(void *p_token, void *p_instance, GDNativeBool p_reference) { \
+		return true;                                                                                               \
+	}                                                                                                              \
+	static constexpr GDNativeInstanceBindingCallbacks ___binding_callbacks = {                                     \
+		___binding_create_callback,                                                                                \
+		___binding_free_callback,                                                                                  \
+		___binding_reference_callback,                                                                             \
+	};
+
+// Don't use this for your classes, use GDCLASS() instead.
+#define GDNATIVE_CLASS(m_class, m_inherits)                                                                        \
+private:                                                                                                           \
+	void operator=(const m_class &p_rval) {}                                                                       \
+                                                                                                                   \
+protected:                                                                                                         \
+	virtual const char *_get_class() const override {                                                              \
+		return get_class_static();                                                                                 \
+	}                                                                                                              \
+                                                                                                                   \
+	virtual const GDNativeInstanceBindingCallbacks *_get_bindings_callbacks() const override {                     \
+		return &___binding_callbacks;                                                                              \
+	}                                                                                                              \
+                                                                                                                   \
+	m_class(const char *p_godot_class) : m_inherits(p_godot_class) {}                                              \
+	m_class(GodotObject *p_godot_object) : m_inherits(p_godot_object) {}                                           \
+                                                                                                                   \
+	static void (*_get_bind_methods())() {                                                                         \
+		return nullptr;                                                                                            \
+	}                                                                                                              \
+                                                                                                                   \
+public:                                                                                                            \
+	static void initialize_class() {}                                                                              \
+                                                                                                                   \
+	static const char *get_class_static() {                                                                        \
+		return #m_class;                                                                                           \
+	}                                                                                                              \
+                                                                                                                   \
+	static const char *get_parent_class_static() {                                                                 \
+		return #m_inherits;                                                                                        \
+	}                                                                                                              \
+                                                                                                                   \
+	static void *___binding_create_callback(void *p_token, void *p_instance) {                                     \
+		return memnew(m_class((GodotObject *)p_instance));                                                         \
+	}                                                                                                              \
+	static void ___binding_free_callback(void *p_token, void *p_instance, void *p_binding) {                       \
+		Memory::free_static(reinterpret_cast<m_class *>(p_binding));                                               \
+	}                                                                                                              \
+	static GDNativeBool ___binding_reference_callback(void *p_token, void *p_instance, GDNativeBool p_reference) { \
+		return true;                                                                                               \
+	}                                                                                                              \
+	static constexpr GDNativeInstanceBindingCallbacks ___binding_callbacks = {                                     \
+		___binding_create_callback,                                                                                \
+		___binding_free_callback,                                                                                  \
+		___binding_reference_callback,                                                                             \
+	};                                                                                                             \
+	m_class() : m_class(#m_class) {}
+
+#endif // ! GODOT_CPP_WRAPPED_HPP
